71ef7824b8af820afb6478eced9facc7e0e3521e
ethanray123 <ethanray19@gmail.com>
2017-03-11 22:01:48 -0800

    removed buttons made <a> tags look like btn

--- navbar.php
+++ navbar.php
@@ -16,17 +16,11 @@
 							</div>
 							
 							<div class="nav col-lg-2 col-lg-offset-3 col-md-offset-1" id="options">
-								<button type="button" class="btn btn-default btn-lg">
-									<a href="profile.php"><span class="glyphicon glyphicon-user" aria-hidden="true" aria-label="User"></span></a>
-								</button>
+								<a class="btn btn-default btn-lg" href="profile.php"><span class="glyphicon glyphicon-user" aria-hidden="true" aria-label="User"></span></a>
 								
-								<button type="button" class="btn btn-default btn-lg">
-									<span class="glyphicon glyphicon-globe" aria-hidden="true" aria-label="Notifications"></span>
-								</button>
+								<a class="btn btn-default btn-lg" href="#"><span class="glyphicon glyphicon-globe" aria-hidden="true" aria-label="Notifications"></span></a>
 								
-								<button type="button" class="btn btn-default btn-lg">
-									<a href="logout.php"><span class="glyphicon glyphicon-off" aria-hidden="true" aria-label="Log-out"></span></a>
-								</button>
+								<a class="btn btn-default btn-lg" href="logout.php"><span class="glyphicon glyphicon-off" aria-hidden="true" aria-label="Log-out"></span></a>
 							</div>
 						</div>
 					</div>
