10edc006ab4b7bb0d3af2fb668b264f1783af4c4
ethanray123 <ethanray19@gmail.com>
2017-03-17 20:38:17 +0800

    Update dfave.php

--- dfave.php
+++ dfave.php
@@ -11,7 +11,12 @@
 	$qfave = "DELETE FROM favourite WHERE user_id=".$_SESSION["user_id"]." and post_id=".$id;
 	$fave = mysqli_query($conn, $qfave);
 	
-	$favouritequery = mysqli_query($conn,"SELECT COUNT(post_id) FROM favourite WHERE user_id='{$_POST['user_id']}'");
-    $favouritecount = mysqli_fetch_row($favouritequery);
-   	echo ($favouritecount[0]);
+	$userpostsquery = mysqli_query($conn,"SELECT post_id FROM post WHERE author_id='{$_POST['user_id']}'");
+    $totalfaves = 0;
+    while($userposts = mysqli_fetch_assoc($userpostsquery)){
+      $favequery = mysqli_query($conn,"SELECT COUNT(post_id) AS faves FROM favourite WHERE post_id='{$userposts['post_id']}'");
+      $fave = mysqli_fetch_assoc($favequery);
+      $totalfaves += $fave['faves'];
+    }
+    echo $totalfaves;
 ?>
