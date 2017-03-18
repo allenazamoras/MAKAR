87eb5888910b15107f5bef3b48675acff1eea8a4
ethanray123 <ethanray19@gmail.com>
2017-03-18 22:47:52 +0800

    Update afave.php

--- afave.php
+++ afave.php
@@ -7,11 +7,14 @@
 	require("connectdb.php");
 	
 	$id = $_POST["post_id"];
+	$aid = $_POST["author_id"];
+	$title = $_POST["title"];
 	
 	$qfave = "INSERT INTO favourite (user_id, post_id) VALUES ('".$_SESSION["user_id"]."', '".$id."')";
 	$fave = mysqli_query($conn, $qfave);
-
+	$nfave = "INSERT INTO notify (user_id, notification) VALUES ('".$aid."', 'Someone favourited your post: ".$title."')";
    	$userpostsquery = mysqli_query($conn,"SELECT post_id FROM post WHERE author_id='{$_POST['user_id']}'");
+
     $totalfaves = 0;
     while($userposts = mysqli_fetch_assoc($userpostsquery)){
       $favequery = mysqli_query($conn,"SELECT COUNT(post_id) AS faves FROM favourite WHERE post_id='{$userposts['post_id']}'");
